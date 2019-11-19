3d52dfb2b6b967206b9acc0609597ae22b84f547
Bruna Catarino <brunacatarino505@gmail.com>
2019-11-19 17:41:05 +0000

    cenas
    format do vue.js e alterado link css no master.blade.php

--- resources/js/vue.js
+++ resources/js/vue.js
@@ -6,37 +6,36 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('../../../../../Users/ZenBook/Downloads/05_Laravel_Vue_Updated_Base/resources/js/bootstrap');
+require("../../../../../Users/ZenBook/Downloads/05_Laravel_Vue_Updated_Base/resources/js/bootstrap");
 
-window.Vue = require('resources/js/vue');
-import VueRouter from 'vue-router'
+window.Vue = require("resources/js/vue");
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-import Users from '../../../../../Users/ZenBook/Downloads/05_Laravel_Vue_Updated_Base/resources/js/components/user'
-import Departments from '../../../../../Users/ZenBook/Downloads/05_Laravel_Vue_Updated_Base/resources/js/components/department'
+import Users from "../../../../../Users/ZenBook/Downloads/05_Laravel_Vue_Updated_Base/resources/js/components/user";
+import Departments from "../../../../../Users/ZenBook/Downloads/05_Laravel_Vue_Updated_Base/resources/js/components/department";
 
-Vue.component('user', Users)
+Vue.component("user", Users);
 
 const routes = [
     {
-        path: './', component:Users
+        path: "./",
+        component: Users
     },
     {
-        path: './', component:Departments
+        path: "./",
+        component: Departments
     }
-]
+];
 
-const router =new VueRouter({
+const router = new VueRouter({
     routes
-})
+});
 
 const app = new Vue({
-    el: '#app',
+    el: "#app",
     router,
-    data: {
-    },
-    methods: {
-
-    }
+    data: {},
+    methods: {}
 });
